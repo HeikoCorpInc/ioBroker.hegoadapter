774b77c157d10fc52030e8177403739104e70ffe
HeikoCorpInc <heikocorpinc@gmx.de>
2020-10-04 00:41:35 +0200

    master

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"hegoadapter adapter settings": {
+		"en": "Adapter settings for hegoadapter",
+		"de": "Adaptereinstellungen für hegoadapter",
+		"ru": "Настройки адаптера для hegoadapter",
+		"pt": "Configurações do adaptador para hegoadapter",
+		"nl": "Adapterinstellingen voor hegoadapter",
+		"fr": "Paramètres d'adaptateur pour hegoadapter",
+		"it": "Impostazioni dell'adattatore per hegoadapter",
+		"es": "Ajustes del adaptador para hegoadapter",
+		"pl": "Ustawienia adaptera dla hegoadapter",
+		"zh-cn": "hegoadapter的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "option1",
+		"ru": "option1",
+		"pt": "option1",
+		"nl": "option1",
+		"fr": "option1",
+		"it": "option1",
+		"es": "option1",
+		"pl": "option1",
+		"zh-cn": "option1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "option2",
+		"ru": "option2",
+		"pt": "option2",
+		"nl": "option2",
+		"fr": "option2",
+		"it": "option2",
+		"es": "option2",
+		"pl": "option2",
+		"zh-cn": "option2"
+	}
+};
